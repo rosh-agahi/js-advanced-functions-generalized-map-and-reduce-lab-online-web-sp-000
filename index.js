d0961ef60afee951cf0b07759210f6628e341c2c
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 21:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function map(sourceArray, func) {
     let mapped = sourceArray.map(func);
     return mapped;
 }
+
+function reduce(sourceArray, func, start=0){
+    let reduced = sourceArray.reduce(func, start)
+    return reduced
+}
