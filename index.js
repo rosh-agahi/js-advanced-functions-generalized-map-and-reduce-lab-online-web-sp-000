5d346378d9b41c6ce5c227006b23a31b2d2c3f67
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 21:18:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+function map(sourceArray, func) {
+    let mapped = sourceArray.map(func);
+    return mapped;
+}
+
+function reduce(sourceArray, func, start=0){
+    if (sourceArray.includes(true)){
+        start = true
+    }
+    let reduced = sourceArray.reduce(func, start)
+    return reduced
+}
